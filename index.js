db879d9888d136ae8699b9ea16c13bf246777cc8
Joshua M Loran <joshua.loran@gmail.com>
2018-12-09 22:17:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,32 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1
+  var itemObj = {itemName: item, itemPrice: price}
+  cart.push(itemObj)
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var mainString = "In your cart, you have"
+  var subString = ""
+  if(cart.length===0){
+    return "Your shopping cart is empty."
+  }else{
+    if (cart.length ===1){
+      mainString += ` ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    }
+    //
+    /*for (var i = 0; i < cart.length; i++) {
+      if(i < cart.length){
+      mainString += ` ${cart[i].key} at ${cart[i].value},`}
+      else {mainString += ` and ${cart[i].key} at ${cart[i].value}.`
+        
+      }
+    }*/
+    
+  }
+  return mainString
 }
 
 function total() {
